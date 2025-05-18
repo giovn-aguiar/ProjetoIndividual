dddb5b420a85b6fe3906e8fca016673db98ee0d9
giovanna <giovanna.aguiar@sptech.school>
2025-05-17 22:07:07 -0300

    estilizando a dashboard

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,29 +1,12 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
 CREATE DATABASE underline;
-
 USE underline;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	email VARCHAR(50) NOT NULL UNIQUE,
+	senha VARCHAR(50) NOT NULL,
+    pontuacao INT
 );
 
 CREATE TABLE aviso (
@@ -34,42 +17,31 @@ CREATE TABLE aviso (
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE frases (
+	idFrase INT PRIMARY KEY AUTO_INCREMENT, 
+    descricao VARCHAR(300) NOT NULL
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+CREATE TABLE logUsuario(
+	idLog INT PRIMARY KEY AUTO_INCREMENT,
+	fkUsuario INT,
+	dataHorario DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
 );
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fkn_empresa) values ('Aquário de Peixe-dourado', 2);
 
 CREATE TABLE resultados (
     id INT PRIMARY KEY AUTO_INCREMENT,
     id_usuario INT,
+    fk_frase INT,
     palavras INT,
     acertos INT,
     erros INT,
     tempo INT,
 	ppm INT,
-    data_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    momento DATETIME DEFAULT CURRENT_TIMESTAMP,
+    desempenho DECIMAL(5,2),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
+
+
 
